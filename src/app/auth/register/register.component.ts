8bc186007b97d8891689e50b1e0db292030e03ae
sviatly <filip.swiatly@yahoo.com>
2025-01-20 21:07:20 +0100

    created form group

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,21 +1,60 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [ReactiveFormsModule, FormsModule, NgIf],
+    templateUrl: './register.component.html',
+    styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+    public signupForm!: FormGroup;
+
     showPassword: boolean = false;
     showConfirmPassword: boolean = false;
 
-  public togglePasswordVisibility(): void {
-    this.showPassword = !this.showPassword;
-  }
+    ngOnInit(): void {
+        this.signupForm = new FormGroup(
+            {
+                username: new FormControl(null, [Validators.required]),
+                email: new FormControl(null, [Validators.required, Validators.email]),
+                password: new FormControl(null, [Validators.required, Validators.minLength(8), this.passwordValidator]),
+                'password-repeat': new FormControl(null, [Validators.required])
+            },
+            { validators: this.passwordsMatchValidator }
+        );
+    }
 
-  public toggleConfirmPasswordVisibility(): void {
-    this.showConfirmPassword = !this.showConfirmPassword;
-  }
+    passwordValidator(control: AbstractControl): { [key: string]: boolean } | null {
+        const password = control.value;
+        const hasUppercase = /[A-Z]/.test(password);
+        const hasLowercase = /[a-z]/.test(password);
+        const hasNumber = /[0-9]/.test(password);
+        const hasSpecialChar = /[\W_]/.test(password);
+
+        if (!hasUppercase || !hasLowercase || !hasNumber || !hasSpecialChar) {
+            return { invalidPassword: true };
+        }
+        return null;
+    }
+
+    passwordsMatchValidator(group: AbstractControl): { [key: string]: boolean } | null {
+        const password = group.get('password')?.value;
+        const passwordRepeat = group.get('password-repeat')?.value; // Corrected here
+
+        if (password !== passwordRepeat) {
+            return { passwordsMismatch: true };
+        }
+        return null;
+    }
+
+    public togglePasswordVisibility(): void {
+        this.showPassword = !this.showPassword;
+    }
+
+    public toggleConfirmPasswordVisibility(): void {
+        this.showConfirmPassword = !this.showConfirmPassword;
+    }
 }
